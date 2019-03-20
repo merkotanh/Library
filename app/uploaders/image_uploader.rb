be8ab9c4400be79045e1230ca86baa08cc26897e
merkotanh <galina.merkotan@faceit-team.com>
2019-03-20 17:13:53 +0200

    changes for heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   if Rails.env.production?
-    config.fog_provider = 'fog/aws'
+    fog_provider = 'fog/aws'
   else
     storage :file
   end
