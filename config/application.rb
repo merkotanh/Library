18fba77f263a4879b6fc9c30d8f552711d277c23
merkotanh <galina.merkotan@faceit-team.com>
2019-03-20 15:43:08 +0200

    3changes for heroku

--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,14 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 require 'rails/mongoid'
-Mongoid.load!(Rails.root.join("config", "mongoid.yml"))
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Mongoid.load!(Rails.root.join("config", "mongoid.yml"))
 module Library
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
